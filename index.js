8f4c639cdd06140b7cfc9a62f6242f5ecabde624
darrin@darrinduncan.com <darrin@darrinduncan.com>
2023-06-17 08:21:28 -0400

    Update to core

--- index.js
+++ index.js
@@ -16,7 +16,12 @@ const connectDB = require('./config/db');
 const app = express();
 
 // Middleware
-app.use(cors('http://localhost:3000'));
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(bodyParser.json());
 
